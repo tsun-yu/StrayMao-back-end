0696e07d7e11cc0af8516fa731ef3db75d5f8f70
krok1029 <krok1029@gmail.com>
2020-10-17 16:01:07 +0800

    init commit

---
+++ src/routes/routesMaster.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// const { route } = require('./routeAdpotion');
+const router = express.Router();
+const app = express();
+router.use('/adoption',require(__dirname +'/adoption/main.js'));
+router.use('/cart',require(__dirname +'/cart/main.js'));
+router.use('/homepage',require(__dirname +'/homepage/main.js'));
+router.use('/membership',require(__dirname +'/membership/main.js'));
+router.use('/social_media',require(__dirname +'/social_media/main.js'));
+router.use('/store',require(__dirname +'/store/main.js'));
+router.get('/', (req, res)=>{
+    res.send('there is route master')
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
---
+++ src/routes/social_media/main.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const db = require(__dirname + '/../../db_connect2');
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.send('routes_social_media')
+});
+
+//exapmle for get data from database
+router.get('/try-db', (req, res)=>{
+    db.query('SELECT * FROM address_book LIMIT 2')
+    .then(([results])=>{
+        res.json(results);
+    })
+});
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
