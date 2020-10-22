809243345f50add03cc3758d3f205202744836d6
krok1029 <krok1029@gmail.com>
2020-10-22 09:20:50 +0800

    adoption api update

--- src/routes/adoption/main.js
+++ src/routes/adoption/main.js
@@ -15,6 +15,19 @@ router.get('/try-db', (req, res)=>{
     })
 });
 
+router.get('/get_pet_list/:', (req, res)=>{
+    db.query(`SELECT a.*,b.detailId,c.des 
+              FROM petInfo a 
+              JOIN petDetail b  on a.petId =b.petId  
+              JOIN tagList c on b.tagId = c.tagId and c.typeId=1 
+              WHERE 1`)
+    .then(([results])=>{
+        //還要做資料整理 把同id的動物的tag變成array
+        res.json(results);
+    })
+});
+
+
 
 module.exports = router;
 
