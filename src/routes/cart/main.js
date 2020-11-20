77d9a2f170b72d4faf22efe3832f9cb337b1e96b
Angel <angel73123@gmail.com>
2020-11-20 13:18:57 +0800

    1120

--- src/routes/cart/main.js
+++ src/routes/cart/main.js
@@ -95,18 +95,21 @@ let orderIdOnCardUpdate=0
 let result2=0
 //按下購買 改變數量 buyNow 新增一筆訂單
 router.post('/cartupdate', (req, res) => {
-    // console.log('req.body',req.body);
+    console.log('req.body',req.body);
      const url = `INSERT INTO orderlist (memberId, totalPrice, memberName, address, mobile, orderState, productDelivery, paymentTerm ) 
      VALUES (${req.body.value[0].memberId}, ${0}, '${req.body.value[0].memberName}', '${req.body.value[0].address}', '${req.body.value[0].mobile}', '未出貨', '', '') `;
     db.query(url).then(([results]) => { 
+      console.log('INSERT INTO orderlist', results)
         const url = `select orderId from orderlist order by createAt desc`;
        return db.query(url)
     }).then(([results]) => { 
+      console.log('select orderId from orderlist',results)
         let id = "("+req.body.cartId.join(",")+")"
         const url = `update cartlist set orderId = ${results[0].orderId} where cartId in ${id}`;
         orderIdOnCardUpdate = results[0].orderId
        return db.query(url)
     }).then(([results]) => {
+      console.log('update cartlist set orderId',results)
       // result2 = results
       for (let i = 0; i < req.body.value.length; i++ ){
         const url = `UPDATE cartlist SET buyNow=${1}, quantity=${req.body.value[i].quantity} WHERE cartId=${req.body.value[i].cartId}`;
@@ -114,12 +117,17 @@ router.post('/cartupdate', (req, res) => {
       }
       return [results]
     }).then(([results]) => { 
+      
+      console.log('UPDATE cartlist SET buyNow=${1} results',results)
+
+      console.log('UPDATE cartlist SET buyNow=${1} orderIdOnCardUpdate',orderIdOnCardUpdate)
       const url = `SELECT cartlist.orderId, cartlist.cartId, cartlist.goodsId, cartlist.memberId, cartlist.name, cartlist.goodsImgs, cartlist.price, cartlist.quantity, cartlist.createAt, cartlist.orderId, cartlist.isBuy, cartlist.buyNow, cartlist.orderState, memberlist.memberName, memberlist.mobile, memberlist.address FROM cartlist LEFT JOIN memberlist ON cartlist.memberId=memberlist.memberId WHERE cartlist.orderId=${orderIdOnCardUpdate} and cartlist.buyNow=1 `
       console.log("SQL :", url) 
       return db.query(url)
       // res.json({ data: result2, results: 'success' });
 
       }).then(([results]) => {
+        console.log('SELECT cartlist.orderId, cartlist.cartId, cartlist.goodsId',results)
         res.json({ data: results, results: 'success' });
       });
       
